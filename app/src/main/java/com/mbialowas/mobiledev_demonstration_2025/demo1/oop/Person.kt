ce45a9151a81a2873ad0028747b3498c067fe8ce
RRC-Rence <cmayores@rrc.ca>
2025-01-09 12:49:20 -0600

    Finished the class demo today

--- app/src/main/java/com/mbialowas/mobiledev_demonstration_2025/demo1/oop/Person.kt
+++ app/src/main/java/com/mbialowas/mobiledev_demonstration_2025/demo1/oop/Person.kt
@@ -1,5 +1,22 @@
 package com.mbialowas.mobiledev_demonstration_2025.demo1.oop
 
 class Person {
+    var name: String = "Rence Mayores"
 
+    //getter and setter
+    get() = field //field is the backing field
+    set(value) {
+        field = value
+    } // setter
+
+    // This is what to use *******************
+    var age: Int = 23
+        get() = field
+        set(value) {field = value}
+}
+
+fun main() {
+    var rence = Person()
+    println(rence.name)
+    println(rence.age)
 }
